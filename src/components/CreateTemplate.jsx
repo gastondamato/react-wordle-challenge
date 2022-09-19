5f3204612f52c9e74fc1867ec9470a592f57ab10
Gaston D'Amato <gmdamato@gmail.com>
2022-09-19 21:07:41 +0200

    Update CreateTemplate.jsx

--- src/components/CreateTemplate.jsx
+++ src/components/CreateTemplate.jsx
@@ -1,12 +1,23 @@
-function createDivs(value) {
+import { nanoid } from "nanoid";
+
+function createDivs(value, line) {
+  let index = value.length;
+  const limiter = (line - 1) * 5;
   let divs = [];
+  let counter = 0;
   for (let i = 0; i < 5 * 6; i++) {
+    counter++;
     let divName = value[i] ? `box box-${i} ${value[i].status}` : `box box-${i}`;
-    divs.push(<div className={divName}>{value[i] ? value[i].letter : ""}</div>);
+    divName += index === counter && index > limiter ? " doScale" : "";
+    divs.push(
+      <div key={nanoid()} className={divName}>
+        {value[i] ? value[i].letter : ""}
+      </div>
+    );
   }
   return divs;
 }
 
-export default function CreateTemplate({ value }) {
-  return <div className="box-container">{createDivs(value)}</div>;
+export default function CreateTemplate({ value, line }) {
+  return <div className="box-container">{createDivs(value, line)}</div>;
 }
