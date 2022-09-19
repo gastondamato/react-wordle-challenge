51e50ec8286436c8dcc9ab9c0e6e2ebaa1689d7d
Gaston D'Amato <gmdamato@gmail.com>
2022-09-19 10:59:44 +0200

    adds definition

--- src/components/Looser.jsx
+++ src/components/Looser.jsx
@@ -1,11 +1,13 @@
-export default function Looser() {
+export default function Looser({ word, def }) {
   function refreshPage() {
     window.location.reload(false);
   }
   return (
     <section className="looser">
-      <h2 className="red">LOOOOOOOOSERRRRR!</h2>
-      <p>6 chances wasn't enough?</p>
+      <h2 className="red">You Loose...</h2>
+      <p>The word was:</p>
+      <h2 className="largebox correct">{word}</h2>
+      <p className="definition">{def}</p>
       <button onClick={refreshPage}>TRY AGAIN</button>
     </section>
   );
