7d7e6c901ec86754be2fbc62b06bdfb3ad51587c
Gaston D'Amato <gmdamato@gmail.com>
2022-10-06 20:26:02 +0200

    Update Looser.jsx
    show points

--- src/components/Looser.jsx
+++ src/components/Looser.jsx
@@ -1,13 +1,26 @@
-export default function Looser({ word, def }) {
-  function refreshPage() {
-    window.location.reload(false);
+import React, { useEffect } from "react";
+
+export default function Looser({ word, def, points }) {
+  function refreshPage(event) {
+    if (event.key === "Enter") {
+      window.location.reload(false);
+    }
   }
+  useEffect(() => {
+    document.addEventListener("keydown", refreshPage);
+    return () => {
+      document.removeEventListener("keydown", refreshPage);
+    };
+  }, []);
   return (
     <section className="looser">
       <h2 className="red">You Loose...</h2>
       <p>The word was:</p>
       <h2 className="largebox correct">{word}</h2>
-      <p className="definition">{def}</p>
+      <h2>
+        You made only <b>{points}</b> points
+      </h2>
+      <p className="definition">"{def}"</p>
       <button onClick={refreshPage}>TRY AGAIN</button>
     </section>
   );
