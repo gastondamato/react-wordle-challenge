f9439fce359d3699b229f7b4f76df8eba3824d52
Gaston D'Amato <gmdamato@gmail.com>
2022-10-06 20:28:36 +0200

    Update Winner.jsx
    show points

--- src/components/Winner.jsx
+++ src/components/Winner.jsx
@@ -1,11 +1,25 @@
-export default function Winner({ word, times }) {
-  function refreshPage() {
-    window.location.reload(false);
+import React, { useEffect } from "react";
+
+export default function Winner({ word, times, def, points }) {
+  function refreshPage(event) {
+    if (event.key === "Enter") {
+      window.location.reload(false);
+    }
   }
+  useEffect(() => {
+    document.addEventListener("keydown", refreshPage);
+    return () => {
+      document.removeEventListener("keydown", refreshPage);
+    };
+  }, []);
   return (
     <section className="winner">
       <h2 className="green">YOU WIN IN {times - 1} TRIES!</h2>
+      <h2>
+        Total Points: <b>{points}</b>
+      </h2>
       <h2 className="largebox correct">{word}</h2>
+      <p className="definition">"{def}"</p>
       <button onClick={refreshPage}>TRY AGAIN</button>
     </section>
   );
